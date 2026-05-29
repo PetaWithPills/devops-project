bca24a01015100a0b05a1a5a47c15077b2e6d08b
Zhenya <zefirkawithpills@gmail.com>
2026-05-29 12:02:06 +1000

    Описание твоих изменений

---
+++ server/init/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (name, email)
+VALUES
+('Alex', 'alex@test.com'),
+('John', 'john@test.com');
